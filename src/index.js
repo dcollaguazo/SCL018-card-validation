cd58ca2cd4f513804cdb606a7f9caa47357846d4
Daniela Collaguazo <dcct84@gmail.com>
2021-09-08 12:27:01 -0400

    Luhn algorithm and maskify implemente

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import validator from './validator.js';
 
-console.log(validator);
+let creditCardNumber = document.getElementById('creditCardNumber');
+
+document.getElementById("btnValidate").addEventListener("click", () => {
+    const valid = validator.isValid(creditCardNumber.value);
+    let masked = '';
+    if (valid){
+        alert('Your card is valid!');
+        masked = validator.maskify(creditCardNumber.value);
+    }else{
+        alert('Please enter a valid credit card');
+    }
+    console.log(masked);
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,41 @@
 const validator = {
-  // ...
+  isValid: (creditCardNumber) => {
+    let arrForLoom = creditCardNumber.split("").reverse();
+
+    arrForLoom = arrForLoom.map(Number);
+
+    for (let i = 1; i < arrForLoom.length; i += 2) {
+      // take every second element and multiply it by 2
+      arrForLoom[i] = arrForLoom[i] * 2;
+
+      if (arrForLoom[i] >= 10) {
+        arrForLoom[i] = arrForLoom[i]
+          .toString()
+          .split("")
+          .map(Number)
+          .reduce(function (a, b) {
+            return a + b;
+          }, 0);
+      }
+    }
+    let sum = arrForLoom.reduce(function (a, b) {
+      return a + b;
+    }, 0);
+
+    const result = sum % 10;
+
+    if (result === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  maskify: (creditCardNumber) => {
+    creditCardNumber = creditCardNumber.replace(/\w(?=\w{4})/g, "#");
+    return creditCardNumber;
+
+  },
 };
 
 export default validator;
